40f1ec4941349e1ba12b8c23fb6e03b3ffccaf91
Subramanian Srinivasan <subrsrin@codeaurora.org>
2019-02-05 19:14:58 -0800

    sakura: Add default BTSAR HAL 1.0 to manifest.xml
    Add default BTSAR HAL 1.0 to manifest.xml
    
    Change-Id: Ic12ab77df4b9ffa7d75a336c55c4f7b4468292ab

--- device.mk
+++ device.mk
@@ -151,6 +151,13 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/configs/audio/mixer_paths_mtp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_mtp.xml
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+    libldacBT_dec
+
+# Boot SPL
+BOOT_SECURITY_PATCH = $(PLATFORM_SECURITY_PATCH)
+
 # Camera
 PRODUCT_PACKAGES += \
     android.frameworks.cameraservice.common@2.0 \
