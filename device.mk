c5c04b666b5c7a80b851affc5af20c75a159c3b2
vitor00almei <pondebatata@outlook.com>
2020-04-16 15:45:13 +0000

    daisy: remove qti prefix on powerhal AOSPA not use this, just LOS

--- device.mk
+++ device.mk
@@ -354,7 +354,7 @@ PRODUCT_DEXPREOPT_SPEED_APPS += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service-qti \
+    android.hardware.power@1.2-service \
     android.hardware.power.stats@1.0-service.mock
 
 PRODUCT_COPY_FILES += \
